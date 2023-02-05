bd23ce107ea4fd385600a6cbaff88dc17a01fa41
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-05 14:43:45 +0300

    Fixed a bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
               <Menu />
-              <div className="gpt3__navbar-menu_container-sign">
+              <div className="gpt3__navbar-menu_container-links-sign">
                 <p>Sign in</p>
                 <button type="button">Sign Up</button>
               </div>
