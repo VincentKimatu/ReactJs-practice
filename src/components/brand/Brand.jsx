bdf3b5158e9eda230a5cf2d29af955b1ca49bfff
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-05 15:14:48 +0300

    Finished brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './Brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <section className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google icon" />
+      </div>
+      <div>
+        <img src={slack} alt="slack icon" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian icon" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox icon" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify icon" />
+      </div>
+    </section>
+  );
 };
 
 export default Brand;
