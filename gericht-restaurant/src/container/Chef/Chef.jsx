fd7b7eefe6a14953252495f67a691048c2732632
Vincent Kimatu <vincentkimatu.dev@gmail.com>
2023-02-14 16:29:31 +0300

    Finished chef section

--- gericht-restaurant/src/container/Chef/Chef.jsx
+++ gericht-restaurant/src/container/Chef/Chef.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Chef.css';
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+const Chef = () => {
+  return (
+    <div className='app__bg app__wrapper section__padding'>
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+        <img src={images.chef} alt='chef' />
+      </div>
+
+      <div className='app__wrapper_info'>
+        <SubHeading title="chef's word" />
+        <h1 className='headtext__cormorant'>What We Believe In</h1>
+        <div className='app__chef-content'>
+          <div className='app__chef-content_quote'>
+            <img src={images.quote} alt='quote' />
+            <p className='p__opensans'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit
+              .
+            </p>
+          </div>
+          <p className='p__opensans'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            iure iusto laboriosam esse iste non aperiam nobis repudiandae amet
+            quos!
+          </p>
+        </div>
+
+        <div className='app__chef-sign'>
+          <p>Kevin Luo</p>
+          <p className='p__opensans'>Chef & Founder</p>
+          <img src={images.sign} alt='sign_image' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chef;
